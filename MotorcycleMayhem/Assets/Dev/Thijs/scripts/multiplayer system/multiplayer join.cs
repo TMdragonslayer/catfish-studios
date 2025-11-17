9345a2716b41e593a4398ecdfdd72d73ee34832e
thijs <tmvangigch@gmail.com>
2025-11-17 10:58:49 +0100

    playerjoin system

---
+++ MotorcycleMayhem/Assets/Dev/Thijs/scripts/multiplayer system/multiplayer join.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class MultiplayerJoin : MonoBehaviour
+{
+    [Header("Setup")]
+    public GameObject playerPrefab;
+    public Transform[] spawnPoints;
+    public int maxPlayers = 4;
+
+    private PlayerInputManager inputManager;
+    [SerializeField] private List<PlayerInput> activePlayers = new List<PlayerInput>();
+
+    void Awake()
+    {
+        inputManager = GetComponent<PlayerInputManager>();
+        inputManager.playerPrefab = playerPrefab;
+        inputManager.joinBehavior = PlayerJoinBehavior.JoinPlayersWhenButtonIsPressed;
+
+        inputManager.playerJoinedEvent.AddListener(OnPlayerJoined);
+        inputManager.playerLeftEvent.AddListener(OnPlayerLeft);
+    }
+
+    void OnDestroy()
+    {
+        inputManager.playerJoinedEvent.RemoveListener(OnPlayerJoined);
+        inputManager.playerLeftEvent.RemoveListener(OnPlayerLeft);
+    }
+
+
+    private void OnPlayerJoined(PlayerInput playerInput)
+    {
+        if (activePlayers.Count >= maxPlayers)
+        {
+            Destroy(playerInput.gameObject);
+            return;
+        }
+
+        activePlayers.Add(playerInput);
+
+        int playerIndex = activePlayers.Count - 1;
+ 
+        if (spawnPoints.Length > 0)
+        {
+            Transform spawn = spawnPoints[playerIndex % spawnPoints.Length];
+            playerInput.transform.position = spawn.position;
+            playerInput.transform.rotation = spawn.rotation;
+        }
+        Debug.Log($"Player {playerIndex + 1} joined with device: {playerInput.devices[0].displayName}");
+    }
+
+    private void OnPlayerLeft(PlayerInput playerInput)
+    {
+        activePlayers.Remove(playerInput);
+        Debug.Log("Player left. Remaining: " + activePlayers.Count);
+    }
+}
